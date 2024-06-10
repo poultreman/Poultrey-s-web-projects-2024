7554c7a72fe1fa4ed5611d306da82a8af15f4ec3
poultreman <Emilam@eastsideprep.org>
2024-06-10 11:07:18 -0700

    Add files via upload

---
+++ final/js/script.js
@@ -0,0 +1,13 @@
+function askQuestion() {
+    var q = prompt('Please rate your experiance on a scale of 1-5')
+    if (q != "null") {
+        alert("thank you for your feedback");
+   
+    } else {
+     null   
+    }
+}
+
+function greeting() {
+   alert("Welcome To My Website!");
+}
